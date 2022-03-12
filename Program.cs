790ca5ad67f933c7d428eefcee17ccdada12486b
GaurangVartak21 <gdvartak@gmail.com>
2022-03-12 14:28:23 +0530

    added uc3

--- Program.cs
+++ Program.cs
@@ -3,20 +3,31 @@ namespace EmployeeWages
 {
     class Program
     {
+        public const int Fulltime = 1;
+        public const int Parttime = 2;
+        public const int Perhour = 20;
         static void Main(string[] args)
         {
-            int Fulltime = 1;
+            int emphrs = 0;
+            int empwages=0;
+
             Random random = new Random();
 
-            int employeestatus = random.Next(0, 2);
-            if (employeestatus == Fulltime)
+            int employeestatus = random.Next(0, 3);
+            switch (employeestatus)
             {
-                Console.WriteLine("Employee is present");
+                case Fulltime:
+                    emphrs = 8;
+                    break;
+               case Parttime:
+                    emphrs = 4;
+                    break;
+               default:
+                    emphrs = 0;
+                    break;
             }
-            else
-            {
-                Console.WriteLine("Employee is absent");
-                }
+            empwages = emphrs*Perhour;
+            Console.WriteLine("Employee Wages: " + empwages);
         }
     }
 }
