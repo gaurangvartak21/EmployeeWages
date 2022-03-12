3ccd21912d3348258980c0c78e682da91d0ed501
GaurangVartak21 <gdvartak@gmail.com>
2022-03-12 14:50:34 +0530

    added uc4

--- Program.cs
+++ Program.cs
@@ -6,28 +6,35 @@ namespace EmployeeWages
         public const int Fulltime = 1;
         public const int Parttime = 2;
         public const int Perhour = 20;
+        public const int Workingdays = 20;
         static void Main(string[] args)
         {
             int emphrs = 0;
             int empwages=0;
+            int totalwages = 0;
 
-            Random random = new Random();
-
-            int employeestatus = random.Next(0, 3);
-            switch (employeestatus)
+            for (int day = 0; day < Workingdays; day++)
             {
-                case Fulltime:
-                    emphrs = 8;
-                    break;
-               case Parttime:
-                    emphrs = 4;
-                    break;
-               default:
-                    emphrs = 0;
-                    break;
+                Random random = new Random();
+
+                int employeestatus = random.Next(0, 3);
+                switch (employeestatus)
+                {
+                    case Fulltime:
+                        emphrs = 8;
+                        break;
+                    case Parttime:
+                        emphrs = 4;
+                        break;
+                    default:
+                        emphrs = 0;
+                        break;
+                }
+                empwages = emphrs * Perhour;
+                totalwages = empwages * Workingdays;
+                Console.WriteLine("Employee Wages: " + empwages);
             }
-            empwages = emphrs*Perhour;
-            Console.WriteLine("Employee Wages: " + empwages);
+            Console.WriteLine("Total employee wages are " + totalwages);
         }
     }
 }
