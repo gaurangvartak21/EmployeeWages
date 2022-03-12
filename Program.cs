5279da71d398019fee92bf20c02f721023aabad9
GaurangVartak21 <gdvartak@gmail.com>
2022-03-12 14:05:23 +0530

    Revert "added uc2"
    This reverts commit eb7fcf650abcb15747ce3ad6a1901f870c9212ac.

--- Program.cs
+++ Program.cs
@@ -3,32 +3,20 @@ namespace EmployeeWages
 {
     class Program
     {
-        public const int Fulltime = 1;
-        public const int Parttime = 2;
-        public const int Perhour = 20;
         static void Main(string[] args)
-        { 
-            int emphrs = 0;
-            int empwages = 0;
+        {
+            int Fulltime = 1;
             Random random = new Random();
 
             int employeestatus = random.Next(0, 2);
-            switch (employeestatus)
+            if (employeestatus == Fulltime)
             {
-                case Fulltime:
-                    emphrs = 8;
-                    break;
-
-                case Parttime:
-                    emphrs = 4;
-                    break;
-
-                default:
-                    emphrs = 0;
-                    break;
+                Console.WriteLine("Employee is present");
             }
-            empwages = emphrs * Perhour;
-            Console.WriteLine("Employee Wage : " + empwages);
-        }  
+            else
+            {
+                Console.WriteLine("Employee is absent");
+                }
+        }
     }
 }
